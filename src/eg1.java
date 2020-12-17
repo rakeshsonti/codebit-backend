1b6d8838fe8979f7bda8c1065ffb0b8374af02a6
rambhajansonti <rambhajansonti0597@gmail.com>
2020-12-17 15:10:59 +0530

    c,cpp,java and python code running with input

---
+++ src/eg1.java
@@ -0,0 +1,7 @@
+public class eg1
+{
+public static void main(String args[])
+{
+System.out.println("helllo world");
+}
+}
---
+++ src/index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const { java, python, c, cpp } = require("compile-run");
+const port = 3000;
+app.listen(port, () => {
+   console.log(`server is listening on port${port} `);
+});
+app.get("/run", (req, res) => {
+   console.log("run end-point is executing.....");
+   //  java.runFile("./src/eg1.java", (err, result) => {
+   //     console.log(result);
+   //     console.log("---------------------");
+   //     console.log(err);
+   //  });
+   const userCode = `
+   import java.io.*;
+  import java.util.*;
+   public class eg1
+  {
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    int arr[]=new int[4];
+  System.out.println("helllo world");
+  System.out.println(num);
+  }
+  }
+  `;
+   //  let resultPromise = java.runSource(userCode, { stdin: "3" });
+   //  resultPromise
+   //     .then((result) => {
+   //        console.log("myerror", result.stderr);
+   //        console.log("myres", result.stdout);
+   //        console.log("memoryusage", result.memoryUsage);
+   //        console.log("cpuusage", result.cpuUsage);
+   //        console.log("error type", result.errorType);
+   //        res.status(200).send(result);
+   //     })
+   //     .catch((err) => {
+   //        res.status(400).send(err);
+   //     });
+   //--------------------------------------------------------------
+
+   // const sourcecode = `print('hello world'); num1=input();  print(num1);`;
+   // const sourcecode = `input1=input();print(input1);`;
+   // let resultPromise = python.runSource(sourcecode, { stdin: "3\n2" });
+   // let resultPromise = python.runFile("./src/eg1.py", { stdin: "3\n2" });
+   // resultPromise
+   //    .then((result) => {
+   //       console.log("mypython---", result);
+   //       res.status(200).send(result);
+   //    })
+   //    .catch((err) => {
+   //       console.log(err);
+   //    });
+   // const sourcecode = `#include <iostream>
+   // using namespace std;
+
+   // int main()
+   // {
+   //     int firstNumber, secondNumber, sumOfTwoNumbers;
+
+   //     cout << "Enter two integers: ";
+   //     cin >> firstNumber >> secondNumber;
+
+   //     // sum of two numbers in stored in variable sumOfTwoNumbers
+   //     sumOfTwoNumbers = firstNumber + secondNumber;
+
+   //     // Prints sum
+   //     cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;
+
+   //     return 0;
+   // }`;
+   // let resultPromise = cpp.runSource(sourcecode, { stdin: "3\n2" });
+   // resultPromise
+   //    .then((result) => {
+   //       console.log("mycpp---", result);
+   //       res.status(200).send(result);
+   //    })
+   //    .catch((err) => {
+   //       console.log(err);
+   //    });
+   const sourcecode = `#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}`;
+   let resultPromise = c.runSource(sourcecode, { stdin: "3\n2" });
+   resultPromise
+      .then((result) => {
+         console.log("my---", result);
+         res.status(200).send(result);
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+});
