baec9c132e49f0ad26db08f558f8f3ce08c01c44
rambhajansonti <rambhajansonti0597@gmail.com>
2021-01-14 19:55:45 +0530

    minor changes

--- Main.java
+++ Main.java
@@ -1,30 +1,43 @@
 import java.io.*;
-import java.util.*;
+import java.util.*; 
 class Main {
-  public static void reverseArray(int arr[],int size)
-  {
-    for(int i=0;i<size/2;i++)
-    {
-      int tmp=arr[i];
-      arr[i]=arr[size-i-1];
-      arr[size-i-1]=tmp;
-    }
-    for(int j=0;j<size-1;j++)
-    {
-      System.out.print(arr[j]+" ");
-    }
-    System.out.print(arr[size-1]);
-  }
-  
-  public static void main (String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int size=sc.nextInt();
-    int arr[]=new int[size];
-    for(int i=0;i<size;i++)
-    {
-      arr[i]=sc.nextInt();
-    }
-    reverseArray(arr,size);
-
-  }
+	static class Node {
+		int value;
+		Node left;
+		Node right;
+		Node(int v) {
+			this.value = v;
+		}
+	}
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		Map<Integer, Node> nodes = new HashMap<>();
+		nodes.put(1, new Node(1));
+		Node BSTRoot = nodes.get(1);
+		for(int i=1;i<(N+1);i++){
+			Node left = new Node(sc.nextInt());
+			Node right = new Node(sc.nextInt());
+			nodes.get(i).left = left.value == -1? null: left;
+			nodes.get(i).right = right.value == -1? null: right;
+			if(left.value != -1) {
+				nodes.put(left.value, left);
+			}
+			if(right.value != -1) {
+				nodes.put(right.value, right);
+			}
+		}
+		printInOrder(BSTRoot);
+		System.out.println();
+	}
+	static void printInOrder(Node root) {
+		if(root == null) {
+			return;
+		}
+		// traverse left
+		printInOrder(root.left);
+		System.out.print(root.value + " ");
+		// traverse right
+		printInOrder(root.right);
+	}
 }
